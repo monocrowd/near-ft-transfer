fab94552aa269313da2a380dcb35978641b93c96
monocrowd <monocrowd@gmail.com>
2021-11-24 01:48:31 +0800

    update login

--- src/App.js
+++ src/App.js
@@ -44,17 +44,7 @@ export default function App() {
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
-        <h1>Welcome to NEAR!</h1>
-        <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
-          below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
-        </p>
+        <h1>Welcome to NEARVEMBER Challenge 6!</h1>
         <p>
           Go ahead and click the button below to try it out:
         </p>
